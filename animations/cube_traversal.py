be400b940d61e93d45eef7d3fe24a028774bd567
OrigamiMarie <origamimarie@gmail.com>
2015-03-02 21:45:28 -0800

    Remove as much hard-coded side-length 4 as possible.

--- animations/cube_traversal.py
+++ animations/cube_traversal.py
@@ -3,43 +3,45 @@ from cube_model import *
 
 class CubeTraversal:
 
-    def __init__(self, blink):
+    def __init__(self, blink, edge_length=4):
         self.blink = blink
+        self.n = edge_length
 
-        black = CubeModel(4, 1)
-        red = CubeModel(4, 1)
-        green = CubeModel(4, 1)
-        blue = CubeModel(4, 1)
-        cyan = CubeModel(4, 1)
-        yellow = CubeModel(4, 1)
-        magenta = CubeModel(4, 1)
-        white = CubeModel(4, 1)
+        black = CubeModel(self.n, 1)
+        red = CubeModel(self.n, 1)
+        green = CubeModel(self.n, 1)
+        blue = CubeModel(self.n, 1)
+        cyan = CubeModel(self.n, 1)
+        yellow = CubeModel(self.n, 1)
+        magenta = CubeModel(self.n, 1)
+        white = CubeModel(self.n, 1)
         
-                                  
-        for i in xrange(0, 4):
-            for j in xrange(0, 4):
-                for k in xrange(0, 4):
+        # Just to shorten the variable name
+        n = self.n-1
+        for i in xrange(0, edge_length):
+            for j in xrange(0, edge_length):
+                for k in xrange(0, edge_length):
                     black.set_pixel(i, j, k, Color(i, j, k))
-                    blue.set_pixel(i, j, 3-k, Color(i, j, 15-k))
-                    green.set_pixel(i, 3-j, k, Color(i, 15-j, k))
-                    cyan.set_pixel(i, 3-j, 3-k, Color(i, 15-j, 15-k))
-                    red.set_pixel(3-i, j, k, Color(15-i, j, k))
-                    magenta.set_pixel(3-i, j, 3-k, Color(15-i, j, 15-k))
-                    yellow.set_pixel(3-i, 3-j, k, Color(15-i, 15-j, k))
-                    white.set_pixel(3-i, 3-j, 3-k, Color(15-i, 15-j, 15-k))
+                    blue.set_pixel(i, j, n-k, Color(i, j, 15-k))
+                    green.set_pixel(i, n-j, k, Color(i, 15-j, k))
+                    cyan.set_pixel(i, n-j, n-k, Color(i, 15-j, 15-k))
+                    red.set_pixel(n-i, j, k, Color(15-i, j, k))
+                    magenta.set_pixel(n-i, j, n-k, Color(15-i, j, 15-k))
+                    yellow.set_pixel(n-i, n-j, k, Color(15-i, 15-j, k))
+                    white.set_pixel(n-i, n-j, n-k, Color(15-i, 15-j, 15-k))
         self.sequence = [white, magenta, yellow, white, cyan, magenta, 
                          red, yellow, cyan, white, red, green, 
                          yellow, black, green, blue, magenta, black, 
                         red, blue, black, blue, cyan, green]
 
         black_hot_corner = [0, 0, 0]
-        blue_hot_corner = [0, 0, 3]
-        green_hot_corner = [0, 3, 0]
-        cyan_hot_corner = [0, 3, 3]
-        red_hot_corner = [3, 0, 0]
-        magenta_hot_corner = [3, 0, 3]
-        yellow_hot_corner = [3, 3, 0]
-        white_hot_corner = [3, 3, 3]
+        blue_hot_corner = [0, 0, n]
+        green_hot_corner = [0, n, 0]
+        cyan_hot_corner = [0, n, n]
+        red_hot_corner = [n, 0, 0]
+        magenta_hot_corner = [n, 0, n]
+        yellow_hot_corner = [n, n, 0]
+        white_hot_corner = [n, n, n]
 
         # There are certainly better ways of doing this.  
         self.hot_corners_sequence = [white_hot_corner, magenta_hot_corner, 
@@ -56,20 +58,17 @@ class CubeTraversal:
                                      cyan_hot_corner, green_hot_corner]
 
     def next_cube(self):
-        cube = CubeModel(4, 1)
+        cube = CubeModel(self.n, 1)
         cube.cube = copy.deepcopy(self.sequence[0].cube)
-        increment_cube = [[[[] for k in xrange(0, 4)]
-                      for j in xrange(0, 4)]
-                      for i in xrange(0, 4)]
+        increment_cube = [[[[] for k in xrange(0, self.n)]
+                      for j in xrange(0, self.n)]
+                      for i in xrange(0, self.n)]
         c = 0
-        ######  Uh-oh, you need to learn about array copying!  
-        ######  You need copies of cubes so that you don't mess up the original.
-        ######  Probably.  There are other crazier options.  
         while 1==1:
             c = (c + 1) % 24
-            for i in xrange(0, 4):
-                for j in xrange(0, 4):
-                    for k in xrange(0, 4):
+            for i in xrange(0, self.n):
+                for j in xrange(0, self.n):
+                    for k in xrange(0, self.n):
                         start_color = cube.get_pixel(i, j, k)
                         end_color = self.sequence[c].get_pixel(i, j, k)
                         color_diffs = [(end_color.r - start_color.r)/12,
@@ -77,10 +76,10 @@ class CubeTraversal:
                                        (end_color.b - start_color.b)/12]
                         increment_cube[i][j][k] = color_diffs
 
-            for s in xrange(0, 12):
-                for i in xrange(0, 4):
-                    for j in xrange(0, 4):
-                        for k in xrange(0, 4):
+            for s in xrange(0, 16-self.n):
+                for i in xrange(0, self.n):
+                    for j in xrange(0, self.n):
+                        for k in xrange(0, self.n):
                             # Modifying color in place, don't need to set it.
                             color = cube.get_pixel(i, j, k)
                             color.r = color.r + increment_cube[i][j][k][0]
