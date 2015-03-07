2f0c2d45f29e1cf98d501164ce476bc4ba2d5d7c
OrigamiMarie <origamimarie@gmail.com>
2015-03-07 13:21:40 -0800

    Pip packaging!

---
+++ hypnocube/__init__.py
@@ -0,0 +1,14 @@
+from hypnocube import demo_runner
+
+def sorting_demo():
+    demo = demo_runner.DemoRunner()
+    demo.color_sorting()
+
+def season_lights_demo():
+    demo = demo_runner.DemoRunner()
+    demo.season_lights()
+
+def cube_traversal_demo():
+    demo = demo_runner.DemoRunner()
+    demo.cube_traversal()
+
---
+++ setup.py
@@ -0,0 +1,71 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='hypnocube',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.1',
+
+    description='Python connector for Hypnocube (www.hypnocube.com)',
+
+    # The project's main homepage.
+    url='https://github.com/OrigamiMarie/py_hypnocube',
+
+    # Author details
+    author='OrigamiMarie',
+    author_email='origamimarie+hypnocube@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ],
+
+    # What does your project relate to?
+    keywords='hypnocube python hypno cube',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(),
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['pyserial', 'crc16'],
+
+    entry_points={
+        'console_scripts': [
+            'hypnocube_sorting_demo=hypnocube:sorting_demo',
+            'hypnocube_season_lights_demo=hypnocube:season_lights_demo',
+            'hypnocube_cube_traversal_demo=hypnocube:cube_traversal_demo',
+        ],
+    },
+
+)
+
